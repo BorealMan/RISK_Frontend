91d903bfd3c9306e7938ba248e785fe0d560b82a
Alen <alengracanin@gmail.com>
2023-09-12 17:36:12 -0400

    refactored to use v loop

--- src/components/players/Players.vue
+++ src/components/players/Players.vue
@@ -1,110 +1,26 @@
 <script setup>
 
+const names = ["Alen", "Ben", "Ryan", "Caleb", "Mike", "Bob"];
+
+const players = names.map((name, i) => ({
+    id: i + 1,
+    name: name
+}));
+
 </script>
 
 <template>
     <div class="container">
-        <div class="player-container">
+        <div class="player-container" v-for="player in players" :key="player.id">
             <div class="avatar">
                 <!-- FontAwesome SVG -->
-                <svg xmlns="http://www.w3.org/2000/svg" height="3em"
-                    viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+                <svg xmlns="http://www.w3.org/2000/svg" height="3em" viewBox="0 0 448 512">
                     <path
                         d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z" />
                 </svg>
             </div>
             <div class="player-details">
-                <h3 id="username">Username</h3>
-                <!-- Troop and country tracker? -->
-                <!-- <div class="stats">
-                    <span id="">T</span> 
-                    <span id="">C</span>
-                </div> -->
-            </div>
-        </div>
-        <div class="player-container">
-            <div class="avatar">
-                <!-- FontAwesome SVG -->
-                <svg xmlns="http://www.w3.org/2000/svg" height="3em"
-                    viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
-                    <path
-                        d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z" />
-                </svg>
-            </div>
-            <div class="player-details">
-                <h3 id="username">Username</h3>
-                <!-- Troop and country tracker? -->
-                <!-- <div class="stats">
-                    <span id="">T</span> 
-                    <span id="">C</span>
-                </div> -->
-            </div>
-        </div>
-        <div class="player-container">
-            <div class="avatar">
-                <!-- FontAwesome SVG -->
-                <svg xmlns="http://www.w3.org/2000/svg" height="3em"
-                    viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
-                    <path
-                        d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z" />
-                </svg>
-            </div>
-            <div class="player-details">
-                <h3 id="username">Username</h3>
-                <!-- Troop and country tracker? -->
-                <!-- <div class="stats">
-                    <span id="">T</span> 
-                    <span id="">C</span>
-                </div> -->
-            </div>
-        </div>
-        <div class="player-container">
-            <div class="avatar">
-                <!-- FontAwesome SVG -->
-                <svg xmlns="http://www.w3.org/2000/svg" height="3em"
-                    viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
-                    <path
-                        d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z" />
-                </svg>
-            </div>
-            <div class="player-details">
-                <h3 id="username">Username</h3>
-                <!-- Troop and country tracker? -->
-                <!-- <div class="stats">
-                    <span id="">T</span> 
-                    <span id="">C</span>
-                </div> -->
-            </div>
-        </div>
-        <div class="player-container">
-            <div class="avatar">
-                <!-- FontAwesome SVG -->
-                <svg xmlns="http://www.w3.org/2000/svg" height="3em"
-                    viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
-                    <path
-                        d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z" />
-                </svg>
-            </div>
-            <div class="player-details">
-                <h3 id="username">Username</h3>
-                <!-- Troop and country tracker? -->
-                <!-- <div class="stats">
-                    <span id="">T</span> 
-                    <span id="">C</span>
-                </div> -->
-            </div>
-        </div>
-        <div class="player-container">
-            <div class="avatar">
-                <!-- FontAwesome SVG -->
-                <svg xmlns="http://www.w3.org/2000/svg" height="3em"
-                    viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
-                    <path
-                        d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z" />
-                </svg>
-            </div>
-            <div class="player-details">
-                <h3 id="username">Username</h3>
+                <h3 :id="'username_' + player.id">{{ player.name }}</h3>
                 <!-- Troop and country tracker? -->
                 <!-- <div class="stats">
                     <span id="">T</span> 
