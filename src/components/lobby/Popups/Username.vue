9f449c842ac4e5d56590c631c87af00828fc9c61
Uphillcarp <87249650+Uphillcarp@users.noreply.github.com>
2023-09-13 16:07:53 -0400

    Update to the modal in the lobby for the username

--- src/components/lobby/Popups/Username.vue
+++ src/components/lobby/Popups/Username.vue
@@ -1,11 +1,29 @@
 <script setup>
+    import Modal from '../../modal/Modal.vue'
+    import { ref } from 'vue'
+
+    const username = ref ("");
 
 </script>
 
 <template>
-    <h1>Hello User</h1>
+    <h1>I'm Lobby</h1>
+    <Modal>
+        <div class="userprompt">
+            <h1>Please Enter a Username</h1>
+            <input type="text" v-model="username">
+            <!-- <p>{{ username }}</p> -->
+        </div>
+    </Modal>
 </template>
 
 <style scoped>
-
+    .userprompt {
+        background-color: blue;
+        text-align: center;
+        font-size: 25px;
+    }
+    input{
+        padding: 10px;
+    }
 </style>
