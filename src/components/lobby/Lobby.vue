212bc4205ddc0bd5d370925e4ffde9d583b332b9
Uphillcarp <87249650+Uphillcarp@users.noreply.github.com>
2023-09-28 16:56:58 -0400

    Button is there just doesn't do anything

--- src/components/lobby/Lobby.vue
+++ src/components/lobby/Lobby.vue
@@ -11,6 +11,7 @@ const gamestore = GameStore()
 const { Game } = storeToRefs(gamestore)
 
 const players = ref(Game.value.players);
+const err = ref(undefined);
 
 gamestore.socket.on('playerjoined', (res) => {
     console.log(`Lobby - ${JSON.stringify(res.game.players)}`);
@@ -19,6 +20,13 @@ gamestore.socket.on('playerjoined', (res) => {
 })
 
 const MAX_PLAYERS = 6;
+
+function BeginGame(){
+    if (players.length-1 < 3){
+        err.value = "You need at Least 3 Players to start"
+    }
+    
+}
 
 </script>
 
@@ -32,6 +40,10 @@ const MAX_PLAYERS = 6;
             <PlayersLobby :players="players" :emptyslots="MAX_PLAYERS - players.length" :gameID="gamestore.Game.game_id" />
             <Chat :players="players" />
         </div>
+        <div class="buttons">
+            <div class="button" @click="BeginGame">Begin Game</div>
+        </div>
+        <p class="error">{{ err }}</p>
     </div>
 </template>
 
@@ -46,7 +58,7 @@ const MAX_PLAYERS = 6;
     grid-template-columns: 100px 1fr 100px;
     grid-template-areas: "logo logo logo"
         ". lobby-content ."
-        ". . .";
+        ". buttons .";
     color: #F5F5F5;
 }
 
@@ -67,4 +79,32 @@ const MAX_PLAYERS = 6;
 .hidden {
     display: none;
 }
+
+.buttons {
+        padding: 3rem;
+        border-radius: 10px;
+        width: max(400px, 20%);
+        position:absolute;
+        z-index: 20;
+        top:85%;
+        left:50%;
+        transform: translate(-50%, -50%);
+    }
+
+    .button {
+        padding: 2rem;
+        border-radius:5px;
+        background: darkgrey;
+        margin:1rem 0;
+        text-align: center;
+        font-size: 1.5rem;
+        font-weight:700;
+        transition: all 600ms ease;
+    }
+
+    .button:hover {
+        background: grey;
+        cursor: pointer;
+        color:rgb(25, 241, 25);
+    }
 </style>
